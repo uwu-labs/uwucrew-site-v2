2d4ff8bfa6269a78ea03050ecaca18f36f0c3ce4
chase-manning <chase@manning.dev>
2023-05-13 08:27:14 +0100

    :rocket: add nav items

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,10 +2,36 @@ import "./Header.css";
 
 import logo from "../assets/logo.svg";
 
+interface NavItemType {
+  label: string;
+  href: string;
+}
+
+const navItems: NavItemType[] = [
+  {
+    label: "Art Gallery",
+    href: "/derivatives",
+  },
+  {
+    label: "Dressing Room",
+    href: "/dressing-room",
+  },
+];
+
 const Header = () => {
   return (
     <div className="header">
-      <img className="logo" src={logo} alt="uwucrew logo" />
+      <div className="section">
+        <img className="logo" src={logo} alt="uwucrew logo" />
+        <div className="nav-items">
+          {navItems.map((navItem) => (
+            <a className="nav-item" href={navItem.href} key={navItem.href}>
+              {navItem.label}
+            </a>
+          ))}
+        </div>
+      </div>
+      <div className="section">meow</div>
     </div>
   );
 };
