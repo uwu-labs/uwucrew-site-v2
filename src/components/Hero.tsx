80255fc72e0cdd9b961a4772ce63e0cf3a0dcc15
chase-manning <chase@manning.dev>
2023-05-13 07:21:52 +0100

    :rocket: add header

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,7 +1,13 @@
+import Header from "./Header";
 import "./Hero.css";
 
 const Hero = () => {
-  return <div className="hero">meow</div>;
+  return (
+    <div className="hero">
+      <Header />
+      meow
+    </div>
+  );
 };
 
 export default Hero;
