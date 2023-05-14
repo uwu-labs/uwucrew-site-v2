eebc9552ba89e70406b959099911db7ee28d6119
chase-manning <chase@manning.dev>
2023-05-14 16:25:26 +0100

    :sparkles: add function to get random uwu

--- src/app/globals.ts
+++ src/app/globals.ts
@@ -1,1 +1,7 @@
 export const OPENSEA = "https://opensea.io/collection/uwucrew";
+
+const PROJECT = "uwucrew-thumbnails";
+const STORAGE_URL = `https://firebasestorage.googleapis.com/v0/b/${PROJECT}/o/`;
+export const uwuImage = (uwuId: number) => {
+  return `${STORAGE_URL}${uwuId.toString()}.png?alt=media`;
+};
