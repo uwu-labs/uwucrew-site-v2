74e729814b9c68f762a3a7ca382ce0fdf6661a0d
chase-manning <chase@manning.dev>
2023-06-10 12:25:16 +0100

    :sparkles: create hero content component

---
+++ src/components/HeroContent.tsx
@@ -0,0 +1,48 @@
+import "./HeroContent.css";
+
+import { OPENSEA_LINK, SOCIALS } from "../app/globals";
+import openseaIcon from "../assets/socials/opensea.svg";
+import Button from "./Button";
+
+const HeroContent = () => {
+  return (
+    <div className="container">
+      <div className="hero-content">
+        <div className="hero-column">
+          <h3 className="hero-content-header">
+            Join uwucrew and immerse yourself in a captivating world of anime
+            art
+          </h3>
+          <div className="socials-container">
+            <div className="socials-text">Find us on</div>
+            {SOCIALS.map((social) => (
+              <a
+                href={social.url}
+                target="_blank"
+                rel="noreferrer"
+                key={social.name}
+              >
+                <img className="social" src={social.image} alt={social.name} />
+              </a>
+            ))}
+          </div>
+        </div>
+
+        <div className="hero-column">
+          <p className="hero-content-text">
+            Find your place in a passionate community of anime art enthusiasts,
+            where artists and collectors can connect over their shared love for
+            anime art!
+          </p>
+          <Button
+            label="View on OpenSea"
+            link={OPENSEA_LINK}
+            icon={openseaIcon}
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HeroContent;
