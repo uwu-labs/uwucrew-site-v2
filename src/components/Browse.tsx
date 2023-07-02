895df07b56215c66e8bfe4e25e8cfea1d0ec3715
chase-manning <chase@manning.dev>
2023-07-02 13:06:18 +0100

    :rocket: add browse screen

--- src/components/Browse.tsx
+++ src/components/Browse.tsx
@@ -1,10 +1,24 @@
+import { useRef } from "react";
 import "./Browse.css";
 import Section from "./Section";
+import useFixedStyles from "../hooks/use-scroll-data";
+import Button from "./Button";
 
 const Browse = () => {
+  const containerRef = useRef<HTMLDivElement>(null);
+  const { fixedStyles, percentScrolled } = useFixedStyles(containerRef);
+
   return (
     <Section id="browse">
-      <div className="browse">Browse</div>
+      <div className="browse" ref={containerRef}>
+        <div className="browse-content" style={fixedStyles}>
+          <h2 className="browse-header">Browse anime art from the community</h2>
+          <Button
+            link="https://uwucrew.art/derivatives"
+            label="View derivative gallery"
+          />
+        </div>
+      </div>
     </Section>
   );
 };
