671338e6fed037d030adf9928e927dc8b9f4ca0c
chase-manning <chase@manning.dev>
2023-07-29 21:44:59 +0100

    :rocket: add hamburger menu

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,8 +2,11 @@ import "./Header.css";
 
 import logo from "../assets/logo.svg";
 import opensea from "../assets/socials/opensea.svg";
+import hamburger from "../assets/ui/hamburger.svg";
+
 import Button from "./Button";
 import { OPENSEA_LINK } from "../app/globals";
+import { useState } from "react";
 
 interface NavItemType {
   label: string;
@@ -22,6 +25,8 @@ const navItems: NavItemType[] = [
 ];
 
 const Header = () => {
+  const [isHamburgerOpen, setIsHamburgerOpen] = useState(false);
+
   return (
     <div className="header">
       <div className="section">
@@ -39,6 +44,45 @@ const Header = () => {
       <div className="header-buy-on-opensea">
         <Button label="Buy on OpenSea" link={OPENSEA_LINK} icon={opensea} />
       </div>
+      <button
+        className="header-hamburger"
+        onClick={() => setIsHamburgerOpen(!isHamburgerOpen)}
+      >
+        <img
+          className="header-hamburger-icon"
+          src={hamburger}
+          alt="hamburger menu"
+        />
+      </button>
+      {
+        // Render hamburger menu
+        isHamburgerOpen && (
+          <>
+            <div className="header-hamburger-menu">
+              {navItems.map((navItem) => (
+                <a
+                  className="header-hamburger-menu-item"
+                  href={navItem.href}
+                  key={navItem.href}
+                >
+                  {navItem.label}
+                </a>
+              ))}
+              <div className="header-hamburger-button-container">
+                <Button
+                  label="Buy on OpenSea"
+                  link={OPENSEA_LINK}
+                  icon={opensea}
+                />
+              </div>
+            </div>
+            <button
+              className="header-drop-shadow"
+              onClick={() => setIsHamburgerOpen(false)}
+            />
+          </>
+        )
+      }
     </div>
   );
 };
