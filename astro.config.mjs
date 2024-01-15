dc344dceaa04f78e949b2ae5ec7cea526ab90fe3
Kiwi <ivanthegreatdev@gmail.com>
2024-01-15 15:56:06 -0500

    Add Analytics  (#46)
    * Try this
    
    * Finish tags

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,16 @@
 import { defineConfig } from "astro/config";
+import partytown from '@astrojs/partytown'
 
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()]
+  integrations: [
+    react(),
+    partytown({
+      config: {
+        forward: ["dataLayer.push"],
+      },
+    })
+  ]
 });
